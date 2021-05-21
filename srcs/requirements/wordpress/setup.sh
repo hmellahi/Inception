1b9ebebef8d00c8494f31118d41cdc501eec961b
hmellahi <hamzasenpai307@gmail.com>
2021-05-21 21:05:29 +0100

    add

--- srcs/requirements/wordpress/setup.sh
+++ srcs/requirements/wordpress/setup.sh
@@ -1,4 +1,8 @@
-apt -y  update
-apt install -y php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+# apt -y  update
+# apt install -y php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
 #service restart nginx
-mkdir /var/www/wordpress
+# chown -R www-data:www-data wordpress \
+# find wordpress/ -type d -exec chmod 750 {} \
+# find wordpress/ -type f -exec chmod 640 {} \
+service php7.3-fpm start
+apt install nmap
